41f2296aaeac8cb279c34a1c45f637307574da17
yuuta-0509 <140583267+yuuta-0509@users.noreply.github.com>
2025-03-23 15:03:40 +0900

    マウスストーカーやら、文字サイズやら

---
+++ script.js
@@ -0,0 +1,4 @@
+const stalker = document.getElementById('mouse-stalker');
+document.addEventListener('mousemove', function (e) {
+    stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
